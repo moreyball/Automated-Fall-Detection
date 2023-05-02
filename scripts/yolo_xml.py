fc3c5069299a697077b0d3a39360261a0cc55deb
Winston <60646400+moreyball@users.noreply.github.com>
2023-05-02 15:15:34 +0800

    Update yolo_xml.py

--- scripts/yolo_xml.py
+++ scripts/yolo_xml.py
@@ -1,49 +1,117 @@
+import xml.etree.ElementTree as ET
+from PIL import Image
 import os
-import argparse
-import xml.etree.cElementTree as ET
+import xml.dom.minidom
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Convert YOLO annotations to Pascal VOC format.')
-    parser.add_argument('--yolo_path', type=str, help='Path to YOLO annotation file')
-    parser.add_argument('--image_path', type=str, help='Path to corresponding image file')
-    parser.add_argument('--output_path', type=str, help='Path to output Pascal VOC XML file')
-    args = parser.parse_args()
-    return args
+def get_image_size(image_path):
+    img = Image.open(image_path)
+    return img.size
 
-def convert_to_xml(yolo_path, image_path, output_path):
-    # Load YOLO annotation file
-    with open(yolo_path, 'r') as f:
-        annotations = f.readlines()
-    
-    # Create XML tree
-    annotation = ET.Element('annotation')
-    
-    # Add filename node
-    filename = ET.SubElement(annotation, 'filename')
-    filename.text = os.path.basename(image_path)
-    
-    # Add size node
-    size = ET.SubElement(annotation, 'size')
-    width, height, _ = cv2.imread(image_path).shape
-    ET.SubElement(size, 'width').text = str(width)
-    ET.SubElement(size, 'height').text = str(height)
-    ET.SubElement(size, 'depth').text = '3'
-    
-    # Add object nodes
-    for annotation in annotations:
-        class_id, x, y, w, h = annotation.split()
-        object_node = ET.SubElement(annotation, 'object')
-        ET.SubElement(object_node, 'name').text = 'class' + class_id
-        ET.SubElement(object_node, 'bndbox')
-        ET.SubElement(bndbox, 'xmin').text = str(int(x) - int(w) // 2)
-        ET.SubElement(bndbox, 'ymin').text = str(int(y) - int(h) // 2)
-        ET.SubElement(bndbox, 'xmax').text = str(int(x) + int(w) // 2)
-        ET.SubElement(bndbox, 'ymax').text = str(int(y) + int(h) // 2)
-    
-    # Write XML tree to file
-    tree = ET.ElementTree(annotation)
-    tree.write(output_path)
 
-if __name__ == '__main__':
-    args = parse_args()
-    convert_to_xml(args.yolo_path, args.image_path, args.output_path)
+def get_image_depth(image_path):
+    img = Image.open(image_path)
+    return len(img.mode.split())
+
+
+def read_file(file_path):
+    with open(file_path, "r", encoding='ISO-8859-1') as f:
+        lines = f.readlines()
+    return lines
+
+
+def calc_xmin(v1, v3, width):
+    xmin = int((float(v1) - float(v3) / 2) * width)
+    return xmin
+
+
+def calc_ymin(v2, v4, height):
+    ymin = int((float(v2) - float(v4) / 2) * height)
+    return ymin
+
+
+def calc_xmax(v1, v3, width):
+    xmax = int((float(v1) + float(v3) / 2) * width)
+    return xmax
+
+
+def calc_ymax(v2, v4, height):
+    ymax = int((float(v2) + float(v4) / 2) * height)
+    return ymax
+
+
+def store_objects(lines, image_path):
+    objects = []
+    width, height = get_image_size(image_path)
+    for line in lines:
+        parts = line.strip().split(" ")
+        obj = {
+            "class": parts[0],
+            "xmin": calc_xmin(parts[1], parts[3], width),
+            "ymin": calc_ymin(parts[2], parts[4], height),
+            "xmax": calc_xmax(parts[1], parts[3], width),
+            "ymax": calc_ymax(parts[2], parts[4], height),
+        }
+        objects.append(obj)
+    return objects
+
+
+def format_xml(file_path):
+    with open(file_path, "r", encoding='ISO-8859-1') as f:
+        xml_data = f.read()
+
+    dom = xml.dom.minidom.parseString(xml_data)
+    xml_formatted = dom.toprettyxml(indent="\t")
+
+    with open(file_path, "w", encoding='ISO-8859-1') as f:
+        f.write(xml_formatted)
+
+
+def write_pascal_voc_xml(image_path, objects, xml_path):
+    image_name = os.path.basename(image_path)
+    root = ET.Element("annotation")
+    ET.SubElement(root, "folder").text = os.path.dirname(image_path)
+    ET.SubElement(root, "filename").text = image_name
+    ET.SubElement(root, "path").text = image_path
+    size = ET.SubElement(root, "size")
+    width, height = get_image_size(image_path)
+    ET.SubElement(size, "width").text = str(width)
+    ET.SubElement(size, "height").text = str(height)
+    ET.SubElement(size, "depth").text = str(get_image_depth(image_path))
+    ET.SubElement(root, "segmented").text = "0"
+    for obj in objects:
+        obj_elem = ET.SubElement(root, "object")
+        ET.SubElement(obj_elem, "name").text = obj["class"]
+        ET.SubElement(obj_elem, "pose").text = "Unspecified"
+        ET.SubElement(obj_elem, "truncated").text = "0"
+        ET.SubElement(obj_elem, "difficult").text = "0"
+        bbox = ET.SubElement(obj_elem, "bndbox")
+        ET.SubElement(bbox, "xmin").text = str(obj["xmin"])
+        ET.SubElement(bbox, "ymin").text = str(obj["ymin"])
+        ET.SubElement(bbox, "xmax").text = str(obj["xmax"])
+        ET.SubElement(bbox, "ymax").text = str(obj["ymax"])
+    tree = ET.ElementTree(root)
+    tree.write(xml_path)
+    format_xml(xml_path)
+
+
+def txt_pascal_voc_main(path):
+    for file in os.listdir(path):
+        if file.endswith(".txt"):
+            file_path = os.path.join(path, file)
+            lines = read_file(file_path)
+
+            jpgfile = os.path.join(path, os.path.splitext(file)[0] + ".jpg")
+            xmlfile = os.path.join(path, os.path.splitext(file)[0] + ".xml")
+            
+            objects = store_objects(lines, jpgfile)
+
+            write_pascal_voc_xml(jpgfile, objects, xmlfile)
+            
+
+if __name__ == "__main__":
+    # PATH CONFIGURATION
+    path1 = "images/test1"
+    path2 = "images/"
+
+    txt_pascal_voc_main(path1)
+    txt_pascal_voc_main(path2)
