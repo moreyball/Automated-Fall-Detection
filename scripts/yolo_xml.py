722c14143c12461768f66a59e337ca9099d89b8d
zlhii <lianghii523@gmail.com>
2023-05-16 15:41:43 +0800

    Update yolo_xml.py

--- scripts/yolo_xml.py
+++ scripts/yolo_xml.py
@@ -118,8 +118,8 @@ def txt_pascal_voc_main(path):
 
 if __name__ == "__main__":
     # PATH CONFIGURATION
-    path1 = "images/test"
-    path2 = "images/train"
+    path1 = "one_step_images/test"
+    path2 = "one_step_images/train"
 
     txt_pascal_voc_main(path1)
     txt_pascal_voc_main(path2)
